f573b4d954618c34b4d57fb708363e7e928bc02e
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-09 23:32:59 +0100

    8

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,28 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+// class Component {
+//   constructor (public props:T) {
 
-  }
+//   }
+// }
+
+// class Page extends Component {
+//   pageInfo () {
+//     console.log(this.props.title);
+//   }
+// }
+
+interface ComponenntProps {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends ComponenntProps> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<ComponenntProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
